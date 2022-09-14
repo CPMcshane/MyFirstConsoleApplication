c6f47ab637ad6325b9fd3f8e71c0f064dde5953c
Conor McShane <Cpmcshane@icloud.com>
2022-09-13 20:59:34 -0600

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿class Person
+{
+    public string? name;
+    public string? location;
+
+    static void Main()
+    {
+        Person a = new Person();
+        a.GetUserNameAndLocation();
+        a.ChristmasCountdown(DateTime.Now);
+        GlazerApp.RunExample();
+    }
+
+    private void GetUserNameAndLocation()
+    {
+        Console.WriteLine("What is your name?");
+        name = Console.ReadLine();
+
+        Console.WriteLine($"Hi {name}! Where are you from?");
+        location = Console.ReadLine();
+
+        Console.WriteLine($"I have never been to {location}. I bet it is nice. Press any key to continue...");
+        Console.ReadKey();
+
+    }
+
+    private void ChristmasCountdown(DateTime date)
+    {
+        Console.WriteLine($"Today's date is {date.Date}");
+
+        int daysTillChristmas = 359 - date.DayOfYear;
+        if (daysTillChristmas < 0) daysTillChristmas += 365;
+
+        Console.WriteLine($"There are {daysTillChristmas} days until Christmas!");
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
